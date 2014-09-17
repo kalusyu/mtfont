d08e4ca8d141e3f0f34c7fcf07f55e9d4d943807
kalus <741470894@qq.com>
2014-09-17 23:44:12 +0800

    sql

--- jfinal_test.sql
+++ jfinal_test.sql
@@ -1,46 +1,45 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : MySQL
-Source Server Version : 50087
+Source Server         : xmlocal
+Source Server Version : 50140
 Source Host           : localhost:3306
 Source Database       : jfinal_test
 
 Target Server Type    : MYSQL
-Target Server Version : 50087
+Target Server Version : 50140
 File Encoding         : 65001
 
-Date: 2014-09-17 20:36:54
+Date: 2014-09-17 23:42:11
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
 -- ----------------------------
 -- Table structure for `deviceinfo`
 -- ----------------------------
 DROP TABLE IF EXISTS `deviceinfo`;
 CREATE TABLE `deviceinfo` (
-  `id` int(11) NOT NULL auto_increment,
-  `imei` varchar(255) default NULL,
-  `macAddress` varchar(255) default NULL,
-  `imsi` varchar(255) default NULL,
-  `product` varchar(255) default NULL,
-  `brand` varchar(255) default NULL,
-  `manufacturer` varchar(255) default NULL,
-  `device` varchar(255) default NULL,
-  `sdk` varchar(255) default NULL,
-  `board` varchar(255) default NULL,
-  `display` varchar(255) default NULL,
-  `host` varchar(255) default NULL,
-  `model` varchar(255) default NULL,
-  `time` varchar(255) default NULL,
-  `androidVersion` varchar(255) default NULL,
-  `telephone` varchar(255) default NULL,
-  `networkType` varchar(255) default NULL,
-  `simOperatorName` varchar(255) default NULL,
-  `simSerialNumber` varchar(255) default NULL,
-  `simState` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `imei` varchar(255) DEFAULT NULL,
+  `macAddress` varchar(255) DEFAULT NULL,
+  `imsi` varchar(255) DEFAULT NULL,
+  `product` varchar(255) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `manufacturer` varchar(255) DEFAULT NULL,
+  `device` varchar(255) DEFAULT NULL,
+  `sdk` varchar(255) DEFAULT NULL,
+  `board` varchar(255) DEFAULT NULL,
+  `display` varchar(255) DEFAULT NULL,
+  `host` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `time` varchar(255) DEFAULT NULL,
+  `androidVersion` varchar(255) DEFAULT NULL,
+  `telephone` varchar(255) DEFAULT NULL,
+  `networkType` varchar(255) DEFAULT NULL,
+  `simOperatorName` varchar(255) DEFAULT NULL,
+  `simSerialNumber` varchar(255) DEFAULT NULL,
+  `simState` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -52,22 +51,38 @@ CREATE TABLE `deviceinfo` (
 -- ----------------------------
 DROP TABLE IF EXISTS `fileinfo`;
 CREATE TABLE `fileinfo` (
-  `id` int(11) NOT NULL auto_increment,
-  `fontDisplayName` varchar(255) default NULL,
-  `fontName` varchar(255) default NULL,
-  `fontNamePic` varchar(255) default NULL,
-  `fontNamePicUri` varchar(255) default NULL,
-  `fontThumnailPic` varchar(255) default NULL,
-  `fontThumnailPicUri` varchar(255) default NULL,
-  `fontSize` varchar(255) default NULL,
-  `fontUri` varchar(255) default NULL,
-  `pictureUri` varchar(255) default NULL,
-  `fontLocalPath` varchar(255) default NULL,
-  `pictureLocalPath` varchar(255) default NULL,
-  `packageName` varchar(255) default NULL,
-  `loveNumbers` varchar(255) default NULL,
-  `downloadNumbers` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fontDisplayName` varchar(255) DEFAULT NULL,
+  `fontName` varchar(255) DEFAULT NULL,
+  `fontNamePic` varchar(255) DEFAULT NULL,
+  `fontNamePicUri` varchar(255) DEFAULT NULL,
+  `fontThumnailPic` varchar(255) DEFAULT NULL,
+  `fontThumnailPicUri` varchar(255) DEFAULT NULL,
+  `fontSize` varchar(255) DEFAULT NULL,
+  `fontUri` varchar(255) DEFAULT NULL,
+  `pictureUri` varchar(255) DEFAULT NULL,
+  `fontLocalPath` varchar(255) DEFAULT NULL,
+  `pictureLocalPath` varchar(255) DEFAULT NULL,
+  `packageName` varchar(255) DEFAULT NULL,
+  `loveNumbers` varchar(255) DEFAULT NULL,
+  `downloadNumbers` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+-- Records of fileinfo
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for `freeuser`
+-- ----------------------------
+DROP TABLE IF EXISTS `freeuser`;
+CREATE TABLE `freeuser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `imei` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of freeuser
+-- ----------------------------
