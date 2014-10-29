99aec907807874b9ab382925685d3f55c1595045
KaluYu <741470894@qq.com>
2014-10-29 23:13:09 +0800

    count calculate

--- src/com/sg/controller/CountController.java
+++ src/com/sg/controller/CountController.java
@@ -1,10 +1,22 @@
 package com.sg.controller;
 
 import com.jfinal.core.Controller;
+import com.sg.model.Count;
 
 public class CountController extends Controller{
 
     public void index(){
         
+    }
+    
+    public void save(){
+    	int fileId = getParaToInt();
+    	Count count = getModel(Count.class);
+    }
+    
+    public void update(){
+    	int fileId = getParaToInt();
+    	Count count = Count.dao.findByFileId(fileId);
+    	count.updateCount();
     }
 }
--- src/com/sg/model/Count.java
+++ src/com/sg/model/Count.java
@@ -1,5 +1,7 @@
 package com.sg.model;
 
+import java.util.List;
+
 import com.jfinal.plugin.activerecord.Model;
 
 /**
@@ -10,6 +12,18 @@ import com.jfinal.plugin.activerecord.Model;
 public class Count extends Model<Count>{
 
     public static Count dao = new Count();
+
+	public void updateCount() {
+		set("loveNum", getInt("loveNum") + 1).set("downloadNum", getInt("downloadNum")+1).update();
+	}
+
+	public Count findByFileId(int fileId) {
+		List<Count> list = find("select * from count where fileId=?",fileId);
+		if (list.size() != 0){
+			return list.get(0);
+		}
+		return null;
+	}
     
     
 }
